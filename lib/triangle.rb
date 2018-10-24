e8e5b839ef2508e0503256b81a6374101d384511
Rose Brennan <brennan.m.rose@gmail.com>
2018-10-24 11:00:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,3 +27,9 @@ class Triangle
   class TriangleError < StandardError 
   end 
 end
+
+  # def validate_triangle
+  #   real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+  #   [a, b, c].each { |s| real_triangle << false if s <= 0 }
+  #   raise TriangleError if real_triangle.include?(false)
+  # end
