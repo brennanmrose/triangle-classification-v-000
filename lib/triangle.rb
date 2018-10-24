c867cc41b98d6697123320b62245c3eaa3ba9b03
Rose Brennan <brennan.m.rose@gmail.com>
2018-10-24 11:07:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
     if @a == @b && @b == @c 
       :equilateral
     elsif
-      @a != @b && @b != @c && @c != @a 
+      @a != @b && @b != @c  
       :scalene
     else 
       :isosceles
